629c04e848d33c9cfa2e8080e265a213c2c56001
Martin <galvezmartin00@gmail.com>
2024-10-07 00:30:33 -0300

    cambnios en pagina de inicio

--- html/index.php
+++ html/index.php
@@ -21,7 +21,7 @@
         <div class="container">
             <h1>ProyectPulse</h1>
             <h2>Tu futuro es acá</h2>
-            <p style="text-align: justify;">Somos la plataforma que facilita la conexión entre talento y empresas.
+            <p >Somos la plataforma que facilita la conexión entre talento y empresas.
                 Encuentra oportunidades laborales en diversas industrias y avanza en tu carrera profesional con
                 nosotros. Únete hoy para descubrir nuevas oportunidades que se alineen con tus habilidades y
                 experiencia.</p>
@@ -35,6 +35,47 @@
         </div>
     </div>
     <div class="body2">
+        <div class="seccion-inscripcion">
+            <h2>¿Te cuesta buscar trabajo?</h2>
+            <p>Nosotros podemos buscar los puestos que necesitas y postularse!</p>
+            <button>Inscripción</button>
+        </div>
+    </div>
+    <div class="body3">
 
+        <section class="testimonios">
+            <div class="testimonio">
+                <div class="stars">★★★★★</div>
+                <p>Hola te quería agradecer hace 2 días los tengo en IG... la estaba pasando mal y ayer buscando, había un laburo cerca de casa y le mandé CV y hoy me llamaron... estoy muy contento, es de muchísima ayuda esta página, gracias.</p>
+            </div>
+            <div class="testimonio">
+                <div class="stars">★★★★★</div>
+                <p>Quería agradecerle, porque gracias a una publicación que hicieron conseguí trabajo, arranco el día jueves, muchísimas gracias, por más gente como ustedes.</p>
+                <span>- René Vallejos</span>
+            </div>
+            <div class="testimonio">
+                <div class="stars">★★★★★</div>
+                <p>Muchísimas gracias por el CV, gracias a ustedes pude conseguir empleo para poder salir adelante con mi familia, muchísimas gracias, son unos genios, se los re agradezco.</p>
+                <span>- Joaquín Ezequiel</span>
+            </div>
+        </section>
+        
+        <section class="servicios">
+            <div class="servicio">
+                <div class="ribbon">SERVICIOS</div>
+                <h3>Reclutamiento</h3>
+                <p>Encontramos el mejor talento para tu empresa.</p>
+            </div>
+            <div class="servicio">
+                <div class="ribbon">SERVICIOS</div>
+                <h3>Capacitación</h3>
+                <p>Ofrecemos programas de formación profesional.</p>
+            </div>
+            <div class="servicio">
+                <div class="ribbon">SERVICIOS</div>
+                <h3>Consultoría</h3>
+                <p>Brindamos asesoramiento en recursos humanos para mejorar tu negocio.</p>
+            </div>
+        </section>
     </div>
 </body>
